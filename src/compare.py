531c9b0f72ae66245c8378a71438abef0e87553a
Shabaz <shabaz.belim@quantiphi.com>
2019-06-12 13:39:25 +0530

    mtcnn face detection add

--- src/compare.py
+++ src/compare.py
@@ -11,18 +11,18 @@ import sys
 import os
 import copy
 import argparse
-import facenet
-import align.detect_face
+import src.facenet
+import src.align.detect_face
 
-def main(args):
+def main(model, image_files, image_size=160, margin=44, gpu_memory_fraction=1.0):
 
-    images = load_and_align_data(args.image_files, args.image_size, args.margin, args.gpu_memory_fraction)
+    images = load_and_align_data(image_files, image_size, margin, gpu_memory_fraction)
     with tf.Graph().as_default():
 
         with tf.Session() as sess:
       
             # Load the model
-            facenet.load_model(args.model)
+            src.facenet.load_model(model)
     
             # Get input and output tensors
             images_placeholder = tf.get_default_graph().get_tensor_by_name("input:0")
@@ -33,19 +33,13 @@ def main(args):
             feed_dict = { images_placeholder: images, phase_train_placeholder:False }
             emb = sess.run(embeddings, feed_dict=feed_dict)
             
-            nrof_images = len(args.image_files)
+            nrof_images = len(image_files)
             
-            print('Images:')
-            for i in range(nrof_images):
-                print('%1d: %s' % (i, args.image_files[i]))
-            print('')
             
             dist = np.sqrt(np.sum(np.square(np.subtract(emb[0,:], emb[1,:]))))
-            print('Distance between images: ', dist)
-            if dist < 1: 
-                print("Same images") 
-            else:
-                print("Different images")
+       
+    return dist
+
             
             
 def load_and_align_data(image_paths, image_size, margin, gpu_memory_fraction):
@@ -54,19 +48,18 @@ def load_and_align_data(image_paths, image_size, margin, gpu_memory_fraction):
     threshold = [ 0.6, 0.7, 0.7 ]  # three steps's threshold
     factor = 0.709 # scale factor
     
-    print('Creating networks and loading parameters')
     with tf.Graph().as_default():
         gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=gpu_memory_fraction)
         sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options, log_device_placement=False))
         with sess.as_default():
-            pnet, rnet, onet = align.detect_face.create_mtcnn(sess, None)
+            pnet, rnet, onet = src.align.detect_face.create_mtcnn(sess, None)
   
     tmp_image_paths=copy.copy(image_paths)
     img_list = []
     for image in tmp_image_paths:
         img = misc.imread(os.path.expanduser(image), mode='RGB')
         img_size = np.asarray(img.shape)[0:2]
-        bounding_boxes, _ = align.detect_face.detect_face(img, minsize, pnet, rnet, onet, threshold, factor)
+        bounding_boxes, _ = src.align.detect_face.detect_face(img, minsize, pnet, rnet, onet, threshold, factor)
         if len(bounding_boxes) < 1:
           image_paths.remove(image)
           print("can't detect face, remove ", image)
@@ -79,24 +72,24 @@ def load_and_align_data(image_paths, image_size, margin, gpu_memory_fraction):
         bb[3] = np.minimum(det[3]+margin/2, img_size[0])
         cropped = img[bb[1]:bb[3],bb[0]:bb[2],:]
         aligned = misc.imresize(cropped, (image_size, image_size), interp='bilinear')
-        prewhitened = facenet.prewhiten(aligned)
+        prewhitened = src.facenet.prewhiten(aligned)
         img_list.append(prewhitened)
     images = np.stack(img_list)
     return images
 
-def parse_arguments(argv):
-    parser = argparse.ArgumentParser()
+# def parse_arguments(argv):
+#     parser = argparse.ArgumentParser()
     
-    parser.add_argument('model', type=str, 
-        help='Could be either a directory containing the meta_file and ckpt_file or a model protobuf (.pb) file')
-    parser.add_argument('image_files', type=str, nargs='+', help='Images to compare')
-    parser.add_argument('--image_size', type=int,
-        help='Image size (height, width) in pixels.', default=160)
-    parser.add_argument('--margin', type=int,
-        help='Margin for the crop around the bounding box (height, width) in pixels.', default=44)
-    parser.add_argument('--gpu_memory_fraction', type=float,
-        help='Upper bound on the amount of GPU memory that will be used by the process.', default=1.0)
-    return parser.parse_args(argv)
+#     parser.add_argument('model', type=str, 
+#         help='Could be either a directory containing the meta_file and ckpt_file or a model protobuf (.pb) file')
+#     parser.add_argument('image_files', type=str, nargs='+', help='Images to compare')
+#     parser.add_argument('--image_size', type=int,
+#         help='Image size (height, width) in pixels.', default=160)
+#     parser.add_argument('--margin', type=int,
+#         help='Margin for the crop around the bounding box (height, width) in pixels.', default=44)
+#     parser.add_argument('--gpu_memory_fraction', type=float,
+#         help='Upper bound on the amount of GPU memory that will be used by the process.', default=1.0)
+#     return parser.parse_args(argv)
 
-if __name__ == '__main__':
-    main(parse_arguments(sys.argv[1:]))
+# if __name__ == '__main__':
+#     main(parse_arguments(sys.argv[1:]))
